bbdc3bb3186f52dbed7eac8b636de4d7a8d02449
Lukas Meili <luk.meili@bluewin.ch>
2019-09-09 07:22:35 +0200

    :bug: Remove uncessar variable is conig.js on client

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,4 @@
 // place the origin Url here, since this is the default client its window location
 //export const originUrl = window.location.origin;
-export const originUrl = "http://localhost:5555";
+//todo might be removed
+export const originUrl = "";
